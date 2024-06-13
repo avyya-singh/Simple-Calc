223faba6410b6b77cd50b3a6c86b24edf54d1555
Avyya Singh <123427740+avyya-singh@users.noreply.github.com>
2024-06-13 19:18:17 -0400

    added util.ts and constant.ts files

---
+++ src/util.ts
@@ -0,0 +1,6 @@
+export const operationsFunc = {
+    '+': (num1: number, num2: number) => num1 + num2,
+    '-': (num1: number, num2: number) => num1 - num2,
+    '×': (num1: number, num2: number) => num1 * num2,
+    '÷': (num1: number, num2: number) => num1 / num2,
+};
